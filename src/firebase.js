03fb09e4a0b9628701132c303a8572ffa9e7f997
cheoljin408 <kcj409@naver.com>
2021-07-12 14:52:24 +0900

    firebase 설치 완료

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCSVtKOC7gYaoMbqKphifELSKUnsCHi-iU",
+    authDomain: "nwitter-1af85.firebaseapp.com",
+    projectId: "nwitter-1af85",
+    storageBucket: "nwitter-1af85.appspot.com",
+    messagingSenderId: "477489658572",
+    appId: "1:477489658572:web:13f9ddc952e5ff294be069"
+  };
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
