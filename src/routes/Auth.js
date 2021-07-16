5733920adfa1a87074134f747ccb20398ffddd4c
cheoljin408 <kcj409@naver.com>
2021-07-16 22:51:47 +0900

    이메일로 회원가입 하기

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { authService } from '../fbase';
 
 const Auth = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [newAccount, setNewAccount] = useState(true);
     const onChange = (event) => {
         const {
             target: {name, value},
@@ -15,15 +17,30 @@ const Auth = () => {
             setPassword(value);
         }
     };
-    const onSubmit = (event) => {
-        event.preventefault();
-    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            let data;
+            if(newAccount) {
+                // new account
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            }
+            else {
+                // log in
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        }
+        catch(error) {
+            console.log(error)
+        }
+    };
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input name='email' type='email' placeholder='Email' required value={email} onChange={onChange} />
                 <input name='password' type='password' placeholder='Password' required value={password} onChange={onChange} />
-                <input type='submit' value='Log In' />
+                <input type='submit' value={newAccount ? 'Create Account' : 'Log In'} />
             </form>
             <div>
                 <button>Continue with Google</button>
