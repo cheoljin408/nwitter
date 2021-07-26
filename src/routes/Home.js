09be1c935f21ba67d76276611e8df6e8a80eac4b
cheoljin408 <kcj409@naver.com>
2021-07-26 17:46:47 +0900

    firestore에서 nweet받아오기 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { dbService } from '../fbase';
 
 const Home = () => {
     const [nweet, setNweet] = useState('');
+    const [nweets, setNweets] = useState([]);
+    const getNeets = async () => {
+        const dbNweets = await dbService.collection('nweets').get();
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    }
+    useEffect(() => {
+        getNeets();
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
@@ -15,12 +29,18 @@ const Home = () => {
         const {target: {value}} = event;
         setNweet(value);
     };
+    console.log(nweets);
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input type='text' placeholder='What`s on your mind?' maxLength={120} value={nweet} onChange={onChange} />
                 <input type='submit' value='Nweet' />
             </form>
+            <div>
+                {nweets.map(nweet => <div key={nweet.id}>
+                    <h4>{nweet.nweet}</h4>
+                </div>)}
+            </div>
         </div>
     );
 }
