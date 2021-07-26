550598c39d50ef01d4bb86d095e8163e74d0178d
cheoljin408 <kcj409@naver.com>
2021-07-26 23:55:03 +0900

    nweets 받아오는 방식 snapshot으로 변경, user object 사용해서 creator 필드 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { dbService } from '../fbase';
 
-const Home = () => {
+const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState('');
     const [nweets, setNweets] = useState([]);
-    const getNeets = async () => {
-        const dbNweets = await dbService.collection('nweets').get();
-        dbNweets.forEach((document) => {
-            const nweetObject = {
-                ...document.data(),
-                id: document.id,
-            };
-            setNweets((prev) => [nweetObject, ...prev]);
-        });
-    }
+    // const getNeets = async () => {
+    //     const dbNweets = await dbService.collection('nweets').get();
+    //     dbNweets.forEach((document) => {
+    //         const nweetObject = {
+    //             ...document.data(),
+    //             id: document.id,
+    //         };
+    //         setNweets((prev) => [nweetObject, ...prev]);
+    //     });
+    // }
     useEffect(() => {
-        getNeets();
+        // getNeets();
+        dbService.collection('nweets').orderBy('createAt', 'desc').onSnapshot(snapshot => {
+            const nweetArray = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            setNweets(nweetArray);
+        });
     }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("nweets").add({
-            nweet,
+            text: nweet,
             createAt: Date.now(),
+            creator: userObj.uid,
         });
         setNweet('');
     };
@@ -29,7 +37,6 @@ const Home = () => {
         const {target: {value}} = event;
         setNweet(value);
     };
-    console.log(nweets);
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -38,7 +45,7 @@ const Home = () => {
             </form>
             <div>
                 {nweets.map(nweet => <div key={nweet.id}>
-                    <h4>{nweet.nweet}</h4>
+                    <h4>{nweet.text}</h4>
                 </div>)}
             </div>
         </div>
