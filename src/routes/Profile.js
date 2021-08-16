b8806dbe3f77b311a258763252a77ce2ab0539d2
cheoljin408 <kcj409@naver.com>
2021-08-16 17:38:44 +0900

    displayName 업데이트 하기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import { authService } from '../fbase';
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const onLogOutClick = () => {
         authService.signOut();
         history.push('/');
     };
+    const onChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        setNewDisplayName(value);
+    };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if(userObj.displayName !== newDisplayName) {
+            const response = await userObj.updateProfile({
+                displayName: newDisplayName,
+            });
+        }
+    };
     return (
         <>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} type="text" placeholder="Display name" value={newDisplayName} />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
